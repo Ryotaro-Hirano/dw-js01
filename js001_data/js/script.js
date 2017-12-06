d435c4b0c19a1d9cb79842ec0b5408fcf99f22f7
Ryotaro Hirano <ryotar0513jedi@gmail.com>
2017-12-06 23:59:43 +0900

    課題js01

---
+++ js001_data/js/script.js
@@ -0,0 +1,15 @@
+/* **********************************************************
+
+	イベントの実行を確認する
+	〜コンソールとアラートで日付を表示しよう〜
+
+*********************************************************** */
+function showAlert() {
+  var getdata = new Date();
+  alert(getdata);
+}
+
+function showConsole() {
+  var getconsole = new Date();
+  console.log(getconsole);
+}
